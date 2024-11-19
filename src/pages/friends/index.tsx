058333187367b5984f2b776056bac5ff805cf3b1
patricksssa78 <patricksssa78@gmail.com>
2024-11-19 21:59:02 +0800

    done friends and me page

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,9 +1,63 @@
-import React,{Suspense}from "react";
+import React from "react";
 
-export default function EarnView() {
+export default function MintVew() {
+  return (
+    <div
+      className="w-screen h-screen bg-center bg-no-repeat bg-contain pt-[60px] relative"
+      style={{ backgroundImage: "url('/src/assets/imgs/friend/bg.png')" }}
+    >
+      <div className="flex items-center justify-between mx-[20px]">
+        <div className="text-white text-3xl font-black font-['Poppins'] uppercase">
+          goldminer
+        </div>
+        <div className="w-[117px] h-[33px] px-2.5 py-[5px] bg-[#e1a8d9] rounded-[100px] shadow border border-black justify-start items-center gap-2.5 inline-flex">
+          <div className="text-center text-[#74d29e] text-[19px] font-bold font-['Poppins'] capitalize">
+            friends
+          </div>
+          <img
+            src="/src/assets/imgs/friend/arrow-right.png"
+            className="w-[16px] h-[17px]"
+            alt=""
+          />
+        </div>
+      </div>
 
-    return   <Suspense fallback={<div>Loading...</div>}>
-        this is earn View
-    </Suspense>;
+      <div className="flex flex-col items-center justify-center mt-[150px]">
+        <div className=" text-[40px] text-[#efac57] font-bold">
+          INVITE FRIENDS
+        </div>
+        <div className=" w-2/3  text-center text-[18px] text-[#000] font-bold">
+          You are your friend will receive bonuses!
+        </div>
+      </div>
 
+      <div className="mt-[10px] w-full flex justify-center items-center">
+        <div
+          className="w-[277px] h-[102px] shadow  flex items-center  justify-center  bg-center bg-no-repeat bg-contain"
+          style={{
+            backgroundImage: "url('/src/assets/imgs/friend/message_bg.png')",
+          }}
+        >
+          <div className="text-center ">
+            <span className="text-black text-sm font-medium font-['Poppins']">
+              For every friend that plays, <br />
+              you both Get
+            </span>
+            <span className="text-[#777cd9] text-sm font-medium font-['Poppins']">
+              {" "}
+              +5,000 FREE!
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center mt-[50%]">
+        <div className="w-[231px] h-[45px] px-[46px] py-2 bg-[#e04936] rounded-[100px] shadow border border-black justify-center items-center gap-2.5 inline-flex">
+          <div className="text-center text-white text-xl font-black font-['Poppins']">
+            Invite a Friend
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,9 +1,110 @@
-import React,{Suspense}from "react";
+import React from "react";
 
-export default function EarnView() {
+export default function MintVew() {
+  return (
+    <div
+      className="w-screen h-screen bg-center bg-no-repeat bg-contain pt-[60px] relative"
+      style={{ backgroundImage: "url('/src/assets/imgs/mint/bg.png')" }}
+    >
+      <div
+        className=" w-full h-[90px] bg-center bg-no-repeat bg-contain  flex items-center justify-center gap-2"
+        style={{ backgroundImage: "url('/src/assets/imgs/mint/title_bg.png')" }}
+      >
+        <img className="w-[42px] h-[42px]" src="/src/assets/imgs/g_icon.png" />
+        <div className="flex flex-col items-start">
+          <div className="text-black text-xs font-bold font-['Poppins']">
+            Total assets
+          </div>
+          <div className="text-[#edad4b] text-3xl font-black font-['Poppins'] uppercase">
+            2,019,012
+          </div>
+        </div>
+      </div>
 
-    return   <Suspense fallback={<div>Loading...</div>}>
-        this is earn View
-    </Suspense>;
+      <div className="flex flex-col items-center justify-center">
+        <img
+          src="/src/assets/imgs/me/avator.png"
+          className="w-[65px] h-[65px]"
+          alt=""
+        />
+        <div className="text-black text-base font-bold font-['Poppins'] capitalize">
+          Mr.Andrew
+        </div>
+        <div className="w-[146.85px] text-black text-xs font-normal font-['Poppins'] capitalize">
+          chen***m@gmail.com
+        </div>
+      </div>
 
+      <div className=" flex flex-col gap-8  absolute bottom-[70px]  w-screen h-[491px] bg-[#fdeeba] rounded-[20px] border border-black px-[20px] pt-[32px]">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src="/src/assets/imgs/me/wallet_icon.png"
+              className="w-[47px] h-[47px]"
+              alt=""
+            />
+            <div className="text-black text-base font-bold font-['Poppins'] leading-tight">
+              My Wallet
+            </div>
+          </div>
+          <img
+            src="/src/assets/imgs/me/arrow-right.png"
+            className="w-[20px] h-[18px]"
+            alt=""
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src="/src/assets/imgs/me/my_orders.png"
+              className="w-[47px] h-[47px]"
+              alt=""
+            />
+            <div className="text-black text-base font-bold font-['Poppins'] leading-tight">
+              My Orders
+            </div>
+          </div>
+          <img
+            src="/src/assets/imgs/me/arrow-right.png"
+            className="w-[20px] h-[18px]"
+            alt=""
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src="/src/assets/imgs/me/account.png"
+              className="w-[47px] h-[47px]"
+              alt=""
+            />
+            <div className="text-black text-base font-bold font-['Poppins'] leading-tight">
+              Account Management
+            </div>
+          </div>
+          <img
+            src="/src/assets/imgs/me/arrow-right.png"
+            className="w-[20px] h-[18px]"
+            alt=""
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src="/src/assets/imgs/me/lang.png"
+              className="w-[47px] h-[47px]"
+              alt=""
+            />
+            <div className="text-black text-base font-bold font-['Poppins'] leading-tight">
+              Language
+            </div>
+          </div>
+          <img
+            src="/src/assets/imgs/me/arrow-right.png"
+            className="w-[20px] h-[18px]"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
