2133530d5ac58240d90c846e643ee70790f45886
patricksssa78 <patricksssa78@gmail.com>
2024-11-19 17:25:26 +0800

    Add me page

---
+++ src/pages/me/index.tsx
@@ -0,0 +1,9 @@
+import React,{Suspense}from "react";
+
+export default function EarnView() {
+
+    return   <Suspense fallback={<div>Loading...</div>}>
+        this is earn View
+    </Suspense>;
+
+}
